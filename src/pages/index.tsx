a4bebfab6ccab9f0ac2e6dd2c73c207821e1c5c4
Walter Woodward <walterwoodward.dev@gmail.com>
2024-09-07 20:55:30 -0700

    updates Home component to render mock data as a list of FoodTruck component instances

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import FoodTruck from '@/components/FoodTruck';
 import mockData from './mockData';
 
 export default function Home() {
@@ -8,8 +9,19 @@ export default function Home() {
       <div className="text-center m-6">
         <h1 className="mb-6 font-extrabold text-3xl text-center">Food Trucks of San Francisco</h1>
       </div>
-      {!!foodTrucks.length && <div className="flex flex-col items-center">
-        {foodTrucks.map((truck: any) => <div>{truck.applicant}</div>)}
+      {!!foodTrucks.length && <div
+        className={`grid items-center justify-items-center min-h-screen p-0`}
+      >
+        <div className="flex flex-col gap-8 row-start-1 items-center sm:items-start">
+          {foodTrucks.map((truck: any) => {
+            return <FoodTruck
+              key={truck.objectid}
+              applicant={truck.applicant}
+              address={truck.locationdescription}
+              description={truck.fooditems}
+            />
+          })}
+        </div>
       </div>}
     </>
   );
