f60276ab5e0c6720b233f20f623a97c40139285b
Walter Woodward <walterwoodward.dev@gmail.com>
2024-09-14 10:00:38 -0700

    post submission commit - replaces img tag with next/image Image component for added performance benefits etc.

--- src/components/FoodTruck.tsx
+++ src/components/FoodTruck.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import { FoodTruckType } from "./types"
 
 // source: https://tw-elements.com/docs/react/components/cards/
@@ -5,9 +6,11 @@ const FoodTruck = ({ applicant, address, description }: FoodTruckType) => {
     return (<>
         <div
             className="flex flex-col rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700 md:max-w-xl md:flex-row">
-            <img
+            <Image
+                width={500}
+                height={500}
                 className="h-96 w-full rounded-t-lg object-cover md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
-                src="https://tecdn.b-cdn.net/img/new/standard/nature/186.jpg"
+                src="/food-truck.jpg"
                 alt="" />
             <div className="flex flex-col justify-start p-6">
                 <h5
